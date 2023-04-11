897fe6e5226af1db830c82ab2d34ffe9e9806581
samuel <samuelnoye35@gmail.com>
2023-04-11 03:33:00 -0700

    #ifndef MAIN_H

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,30 +1,23 @@
-#include "holberton.h"
+#include "main.h"
 #include <stdlib.h>
-
 /**
- * create_array - A function that creates an array of chars,
- * and initializes it with a specific char.
- * @size: The size of the array
- * @c: A character to initialized the array
- * Return: Apointer to the array or NULL if it fails
+ * create_array - create array of size size and assign char c
+ * @size: size of array
+ * @c: char to assign
+ * Description: create array of size size and assign char c
+ * Return: pointer to array, NULL if fail
+ *
  */
 char *create_array(unsigned int size, char c)
 {
-	char *s;
-	unsigned int i = 0;
+	char *str;
+	unsigned int i;
 
-	s = malloc(size * sizeof(char));
-
-	if (s == NULL)
+	str = malloc(sizeof(char) * size);
+	if (size == 0 || str == NULL)
 		return (NULL);
 
-	if (size == 0)
-		return (NULL);
-
-	while (i < size)
-	{
-		s[i] = c;
-		i++;
-	}
-	return (s);
+	for (i = 0; i < size; i++)
+		str[i] = c;
+	return (str);
 }
